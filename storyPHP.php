cd41b3b032d869545d048336ca87d85b74b7a1c0
nelsonleopold <nelsonleopoldiv@guamcc.edu>
2020-09-14 14:05:17 +1000

    madlib homework

---
+++ storyPHP.php
@@ -0,0 +1,24 @@
+<!--
+Name Nelson Leopold
+Date 
+This program does -- 
+-->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <style>
+            body {
+                background-color: lightblue;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Little Boy Who?</h1>
+        <?php
+            echo("Hello World");
+        ?>
+
+    </body>
+</html>
