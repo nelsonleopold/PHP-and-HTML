7b1163032edd76f327492aa916e45d0edefa778f
nelsonleopold <nelsonleopoldiv@guamcc.edu>
2020-09-14 15:04:53 +1000

    Finished homework

--- storyPHP.php
+++ storyPHP.php
@@ -1,7 +1,7 @@
 <!--
 Name Nelson Leopold
-Date 
-This program does -- 
+Date 9/14/2020
+This program does -- This program asks for different catagories of responses from user, then makes a story. Like a madlib. 
 -->
 
 <!DOCTYPE html>
@@ -11,14 +11,18 @@ This program does --
         <style>
             body {
                 background-color: lightblue;
+                text-align: center;
+                font-weight: 900;
             }
         </style>
     </head>
     <body>
         <h1>Little Boy Who?</h1>
-        <?php
-            echo("Hello World");
-        ?>
-
+        Little Boy <?php echo $_POST["color"]?>, come blow your <?php echo $_POST["instrument"]; ?>!<br>
+        The <?php echo $_POST["animal"]?> is in the <?php echo $_POST["place"]?>, the <?php echo $_POST["anotherAnimal"]?> is in the <?php echo $_POST["vegetable"]; ?>.<br>
+        Where's the boy that looks after the <?php echo $_POST["yetAnotherAnimal"]; ?>?<br>
+        He's under the <?php echo $_POST["aStructure"]?>, <?php echo $_POST["action"]?>.<br>
+        <br>
+        End of story!
     </body>
 </html>
